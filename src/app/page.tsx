a819b118c79d47d6e2893bdc196e5ac6705454f1
Felipe Leite <lipi4shared@hotmail.com>
2024-04-11 02:42:16 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,70 @@
+'use client'
+
 import Image from "next/image";
+import Joyride, { Step } from "react-joyride"
+import colors from 'tailwindcss/colors'
+
+  const steps: Step[] = [
+  {
+    target: ".first",
+    content: (
+      <div className="flex flex-col items-center gap-4">
+        <Image src="/woman.jpg" alt="" width={300} height={300} />
+        <p>Esta é a etapa 1.</p>
+      </div>
+    ),
+    title: 'Parte 1',
+    showProgress: true
+  },
+  {
+    target: ".second",
+    title: 'Parte 2',
+    content: (
+      <div className="flex flex-col items-center gap-4">
+        <Image src="/smile.png" alt="" width={100} height={100} />
+        <p>Esta é a etapa 2.</p>
+      </div>
+    ),
+    showProgress: true
+  },
+  {
+    target: ".third",
+    title: 'Parte 3',
+    content: 'Esta é a etapa 3.'
+  }
+];
 
 export default function Home() {
+  // const [runTour, setRunTour] = useState(false)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+      <Joyride 
+        steps={steps}
+        continuous
+        locale={{ back: 'Voltar', close: 'Fechar', last: 'Concluir', next: 'Próximo', open: 'Abrir', skip: 'Pular' }}
+        // run={runTour}
+        styles={{
+          options: {
+            arrowColor: colors.slate[900],
+            backgroundColor: colors.slate[900],
+            textColor: colors.white,
+            beaconSize: 40, // Tamanho do círculo pulsante que aparece sobre o elemento
+            //width: 100 // limite de largura do tooltip
+            primaryColor: colors.yellow[600], //aplicada ao beacon, botão de próximo e de conclusão
+            overlayColor: colors.slate[900]
+          },
+          buttonBack: {
+            color: colors.gray[300]
+          }
+        }}
+      />
+
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <button onClick={() => setRunTour(true)}>Start TOUR</button> */}
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="first fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
@@ -30,7 +90,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="second relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
@@ -59,7 +119,7 @@ export default function Home() {
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="third group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -109,5 +169,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    </>
   );
 }
